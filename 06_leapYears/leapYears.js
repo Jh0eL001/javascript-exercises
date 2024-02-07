a1c4cfa2ddf5918481780da35cced3da71e22303
SharkMan <jhoel821@gmail.com>
2024-02-07 12:17:37 -0400

    finish leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    // n % 4 === 0 => LEAP
+    // n % 400 === 0 %% n % 100 === 0 => LEAP
+    let n = number;
+    if(n % 4 === 0){return true}
+    else if( n % 100 ===0 && n % 100 === 0){return true}
+    else {return false}
 };
 
 // Do not edit below this line
