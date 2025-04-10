027ed814dcddaba472dc6ce0198f9c35e717444c
SharkDev <jhoel821@gmail.com>
2025-04-09 21:55:28 -0400

    refactor: update repeatString function to use recursion and improve test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,7 @@
-const repeatString = function(word, times) {
-    let string = "";
-    for (let i = 0; i < times; i++) {
-      string += word;
-    }
-    return string;
+const repeatString = (word, num) => {
+  if (num < 0) return "ERROR";
+  if (num === 0) return "";
+  return word + repeatString(word, num - 1);
 };
 
 // Do not edit below this line
